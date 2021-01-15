64b82ab8fb341e5d38c20dd53512cc0ddeb3a7c0
sSeongjae91 <ben91429@gmail.com>
2021-01-15 13:40:17 +0900

    MemoApp

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <app-header/>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    AppHeader
+  }
 }
 </script>
 
 <style>
+@import "./style/reset.css";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 560px;
+  margin: 0 auto;
 }
 </style>
