b77ba521e4b68e7de7bd176a31cd83298a392d13
sSeongjae91 <ben91429@gmail.com>
2021-01-15 14:25:24 +0900

    memoform

---
+++ src/components/Memo.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Single File Componen의 기본 형식</div>
+</template>
+
+<script>
+export default {
+  name: '컴포넌트 이름'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/MemoApp.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="memo-app">
+        <memo-form/>
+        <memo/>
+    </div>
+</template>
+<script>
+import MemoForm from './MemoForm'
+import Memo from './Memo'
+
+export default {
+  name: 'MemoMapp',
+  components: {
+    MemoForm,
+    Memo
+  },
+  data () {
+    return {
+      memos: []
+    }
+  },
+  created () {
+    this.memos = localStorage.memos ? JSON.parse(localStorage.memos) : []
+  }
+}
+</script>
